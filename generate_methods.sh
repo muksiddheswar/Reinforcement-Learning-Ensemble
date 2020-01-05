5e516fd49e86f1d0af987ab7ecd9d54d76f833fe
Jan Van den Schilden <jan.vandenschilden@gmail.com>
2020-01-05 20:30:48 +0100

    Added introduction to experiments

--- generate_methods.sh
+++ generate_methods.sh
@@ -1,6 +1,6 @@
 echo > methods.md
 
-for md in .2.methods.Qlearning.md .2.methods.sarsa.md .2.methods.actor-critic.md .2.methods.QVlearning.md .2.methods.ACLA.md .2.methods.majority-voting.md .2.methods.rank-voting.md .2.methods.boltzmann-multiplication.md .2.methods.boltzmann-addition.md
+for md in .2.methods.Qlearning.md .2.methods.sarsa.md .2.methods.actor-critic.md .2.methods.QVlearning.md .2.methods.ACLA.md .2.methods.majority-voting.md .2.methods.rank-voting.md .2.methods.boltzmann-multiplication.md .2.methods.boltzmann-addition.md .2.methods.experiments.md
 do
     cat ${md} >> methods.md
     echo >> methods.md
