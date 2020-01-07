5825ae1af1c2e9010a2c7f30d29a8b2cc39736e7
Jan Van den Schilden <jan.vandenschilden@gmail.com>
2020-01-07 04:36:39 +0100

    added article in one markdown
    made some changes

---
+++ make_markdown.sh
@@ -0,0 +1,9 @@
+cat abstract_draft > article.md
+echo >> article.md
+cat introductionLearningDynamic.md >> article.md
+echo >> article.md
+cat methods.md >> article.md
+echo >> article.md
+cat results.md >> article.md
+echo >> article.md
+cat discussion.md >> article.md
