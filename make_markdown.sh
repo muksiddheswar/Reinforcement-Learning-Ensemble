5f448c184fff310e0aa82dc062f5ff20728896d8
smkj33 <siddhu06ran@gmail.com>
2020-01-07 17:58:07 +0100

    Removed unnecessary files

--- make_markdown.sh
+++
@@ -1,9 +0,0 @@
-cat abstract_draft > article.md
-echo >> article.md
-cat introductionLearningDynamic.md >> article.md
-echo >> article.md
-cat methods.md >> article.md
-echo >> article.md
-cat results.md >> article.md
-echo >> article.md
-cat discussion.md >> article.md
